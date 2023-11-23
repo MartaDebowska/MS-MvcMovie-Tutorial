5370f66e325f27e6a145372d0be2035841d0dc48
Marta Dębowska <debowska.marta@outlook.com>
2023-11-23 21:47:23 +0100

    Added a view (part 3 tutorial)

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -7,21 +7,27 @@ namespace MvcMovie.Controllers
     {
         //
         // GET: /HelloWorld/
-        public string Index() //HTTP endpoint
-        {
-            return "This is my default action...";
-        }
+        //public string Index() //HTTP endpoint
+        //{
+        //    return "This is my default action...";
+        //}
         //
         // GET: /HelloWorld/Welcome 
-        public string Welcome(string name, int ID = 1) //HTTP endpoint
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID {ID}");
-        }
-        
-        
-        //public IActionResult Index()
+        //public string Welcome(string name, int ID = 1) //HTTP endpoint
         //{
-        //    return View();
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID {ID}");
         //}
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
     }
 }
